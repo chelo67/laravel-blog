0701802fb67a6f85989afae3ad709b015307a647
chelo <marceloacampora@gmail.com>
2017-05-28 19:02:02 -0300

    curso11

---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title','Default') | Panel de Administracion</title>
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}" />
+  </head>
+  <body>
+    @include('admin.template.partials.nav')
+    <section>
+      @yield('content')
+    </section>
+    <script src="{{ asset('plugins/jquery/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
+
+
+  </body>
+</html>
